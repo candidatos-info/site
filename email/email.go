c8242a5f4f9470e49665906d3996219f51474da2
abuarquemf <abuarquemf@gmail.com>
2020-10-08 01:26:13 -0300

    chore: fixed lines not beeing breaked

--- email/email.go
+++ email/email.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var (
+const (
 	mimeHeaders = "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 )
 
--- emails.go
+++ emails.go
@@ -9,21 +9,21 @@ import (
 
 func buildProfileAccessEmail(candidate *descritor.CandidateForDB, accessToken string) string {
 	var emailBodyBuilder strings.Builder
-	emailBodyBuilder.WriteString(fmt.Sprintf("Olá, %s!\n", candidate.Name))
+	emailBodyBuilder.WriteString(fmt.Sprintf("Olá, %s!<br>", candidate.Name))
 	emailBodyBuilder.WriteString(fmt.Sprintf("para acessar seu perfil click no link: %s/profile?access_token=%s\n", siteURL, accessToken))
 	return emailBodyBuilder.String()
 }
 
 func buildReportEmail(candidate *descritor.CandidateForDB, report string) string {
 	var emailBodyBuilder strings.Builder
-	emailBodyBuilder.WriteString(fmt.Sprintf("Nova denúcia do candidato %s: ", candidate.Name))
+	emailBodyBuilder.WriteString(fmt.Sprintf("Nova denúcia do candidato %s: <br><br>", candidate.Name))
 	emailBodyBuilder.WriteString(fmt.Sprintf("\n\n%s\n", report))
 	return emailBodyBuilder.String()
 }
 
 func buildContactMessage(messageType, body string) string {
 	var emailBodyBuilder strings.Builder
-	emailBodyBuilder.WriteString(fmt.Sprintf("Nova mensagem do tipo %s: ", messageType))
+	emailBodyBuilder.WriteString(fmt.Sprintf("Nova mensagem do tipo %s: <br>", messageType))
 	emailBodyBuilder.WriteString(fmt.Sprintf("\n\n%s\n", body))
 	return emailBodyBuilder.String()
 }
