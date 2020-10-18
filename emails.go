d605788cb2938eeb98a825840c4ac53ee6272fb5
Daniel Fireman <danielfireman@gmail.com>
2020-10-18 13:09:03 -0300

    Update emails.go

--- emails.go
+++ emails.go
@@ -12,7 +12,9 @@ func buildProfileAccessEmail(candidate *descritor.CandidateForDB, accessToken st
 	Olá, %s!<br><br>
 	Identificamos através dos dados públicos do TSE que você está cadastrado na eleição de %d na cidade de %s no estado de %s como %s.<br>
 	Recebemos sua solicitação para acessar a plataforma candidatos.info e editar seu perfil. Para isso clique no seguinte link (ou copie e cole no navegador): <a src="%s/atualizar-candidatura?access_token=%s">%s/atualizar-candidatura?access_token=%s</a>
-	<br><br><br>Caso tenha recebido este email por engano apenas desconsidere-o.<br>`, candidate.Name, candidate.Year, candidate.City, candidate.State, candidate.Role, siteURL, accessToken)
+	<br><br><br>Caso tenha recebido este email por engano, por favor desconsidere-o.<br>`, candidate.Name, candidate.Year, candidate.City, candidate.State, candidate.Role, siteURL, accessToken)
+	Atenciosamente,
+	candidatos.info
 }
 
 func buildReportEmail(candidate *descritor.CandidateForDB, report string) string {
